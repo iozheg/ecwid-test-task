9b171cd1755a64eb1b66a37c5e48332c686caed6
Aleksandr Netsev <aknetsev@gmail.com>
2025-09-04 22:09:50 +0400

    fix(Breadcrumbs): remove home breadcrumb for home page

--- src/composables/useBreadcrumbs.ts
+++ src/composables/useBreadcrumbs.ts
@@ -3,19 +3,22 @@ const homeItem = {
   disabled: false,
   href: '/',
 };
-const breadcrumbs = ref([homeItem]);
+const breadcrumbs = ref<
+  Array<{ title: string; href: string; disabled: boolean }>
+>([]);
 
 export const useBreadcrumbs = () => {
   function setBreadcrumbs(
     newBreadcrumbs: Array<{ title: string; href: string }>
   ) {
-    breadcrumbs.value = [homeItem];
+    breadcrumbs.value = [];
     breadcrumbs.value.push(
       ...newBreadcrumbs.map((bc, index) => ({
         ...bc,
         disabled: index === newBreadcrumbs.length - 1,
       }))
     );
+    breadcrumbs.value.length > 0 && breadcrumbs.value.unshift(homeItem);
   }
   return {
     breadcrumbs,
